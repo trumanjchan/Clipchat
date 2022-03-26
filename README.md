382cdd522605805c0985accacedb9a3f395bd72d
Truman <trumanjfchan@gmail.com>
2022-03-26 14:09:18 -0700

    Added Nickname and Popping-objects-from-array functionalities
    For users to name themselves and to free up storage when they leave the website.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Used guide for set-up: https://socket.io/get-started/chat#introduction
+Socket.io website for chatting!
+Starting Code: https://socket.io/get-started/chat#introduction
+Nicknames: https://stackoverflow.com/questions/7702461/socket-io-custom-client-id
+Popping: https://stackoverflow.com/questions/10024866/remove-object-from-array-using-javascript
--- index.js
+++ index.js
@@ -5,27 +5,37 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+var clients = [];
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    console.log('Connected: ' + socket.id);
-    const user = socket.id;
-    io.emit('announce-user', user);
+    socket.on('storeClientInfo', function (data) {
+        var clientInfo = new Object();
+        clientInfo.clientId = socket.id;
+        clientInfo.username = data.username;
+        clients.push(clientInfo);
+        console.log(clients);
+        io.emit('announce-user', data.username);
+        console.log('Welcome, ' + data.username + '!');
+
+        socket.on('chat-message', msg => {
+            io.emit('chat-message', data.username + ': ' + msg);
+            console.log(data.username + ': ' + msg);
+        });
+        socket.on('disconnect', () => {
+            clients = clients.filter(person => person.username != data.username);
+            console.log(clients);
+            io.emit('user-left', data.username);
+            io.emit('update-num-users', count);
+            console.log('Disconnected: ' + data.username);
+        });
+    });
 
     const count = io.of("/").sockets.size;
     io.emit('update-num-users', count);
-
-    socket.on('chat-message', msg => {
-        io.emit('chat-message', user + ': ' + msg);
-        console.log(user + ': ' + msg);
-    });
-    socket.on('disconnect', () => {
-        io.emit('user-left', user);
-        io.emit('update-num-users', count);
-        console.log('Disconnected: ' + user);
-    });
 });
 
 server.listen(3000, () => {
